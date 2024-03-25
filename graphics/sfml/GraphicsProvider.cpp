492a09c1cd8a20a14a7d77ff4d37bed95408cfbb
Matheo Coquet <matheo.coquet@epitech.eu>
2024-03-25 18:57:18 +0100

    feat: init sfml provider

---
+++ graphics/sfml/GraphicsProvider.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2024
+** GraphicsProvider.cpp
+** File description:
+** GraphicsProvider class
+*/
+
+#include "GraphicsProvider.hpp"
---
+++ graphics/sfml/GraphicsProvider.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2024
+** GraphicsProvider.hpp
+** File description:
+** GraphicsProvider class
+*/
+
+#pragma once
+
+#include "shared/graphics/IGraphicsProvider.hpp"
+
+class GraphicsProvider : public shared::graphics::IGraphicsProvider {
+
+};
