2eccb5963e5e6cecdd9ca6978610cd1640e51ad3
Yann <yannmasson17@gmail.com>
2024-03-26 19:22:52 +0100

    feat: add cache system for textures

--- core/src/core/Core.cpp
+++ core/src/core/Core.cpp
@@ -8,16 +8,18 @@
 #include <memory.h>
 #include "Core.hpp"
 #include "shared/games/components/IComponent.hpp"
-#include "shared/games/components/IDisplayableComponent.hpp"
 
-Core::Core(GameProviders gameProviders, GraphicsProviders graphicsProviders) {
+Core::Core(GameProviders gameProviders, GraphicsProviders graphicsProviders)
+{
     this->_gameProviders = gameProviders;
     this->_graphicsProviders = graphicsProviders;
+    this->_gameProvider = this->_gameProviders.at(0);
+    this->_graphicsProvider = this->_graphicsProviders.at(0);
 }
 
 Core::~Core() {}
 
-void Core::_setup()
+void Core::_initWindow()
 {
     shared::graphics::WindowInitProps windowInitProps {
         {0, 0},
@@ -27,12 +29,30 @@ void Core::_setup()
         "icon"
     };
 
-    this->_gameProvider = this->_gameProviders.at(0);
-    this->_graphicsProvider = this->_graphicsProviders.at(0);
-
     this->_game = this->_gameProvider.get()->createInstance();
     windowInitProps.size = this->_game.get()->getSize();
     this->_window = this->_graphicsProvider.get()->createWindow(windowInitProps);
+}
+
+std::shared_ptr<shared::graphics::ITexture> Core::_getTexture(std::string bin, std::string ascii)
+{
+    if (this->_textures.find(bin + ascii) == this->_textures.end())
+        this->_textures[bin + ascii] = this->_graphicsProvider.get()->createTexture(bin, ascii);
+    return this->_textures[bin + ascii];
+}
+
+void Core::_renderDisplayableEntity(std::shared_ptr<shared::games::components::IDisplayableComponent> displayable)
+{
+    auto textureProps = displayable.get()->getTextureProps();
+    shared::graphics::EntityTextureProps entityTextureProps{
+        this->_getTexture(textureProps.sources.bin, textureProps.sources.ascii),
+        textureProps.sources.binTileSize,
+        textureProps.origin};
+    shared::graphics::EntityProps entityProps{
+        entityTextureProps,
+        displayable.get()->getSize(),
+        displayable.get()->getPosition()};
+    this->_window.get()->render(entityProps);
 }
 
 void Core::_renderEntities()
@@ -42,28 +62,15 @@ void Core::_renderEntities()
     for (auto &entity : entities) {
         auto components = entity.second.get()->getComponents();
         for (auto &component : components) {
-            if (component.second.get()->getType() == shared::games::components::DISPLAYABLE) {
-                auto displayable = std::dynamic_pointer_cast<shared::games::components::IDisplayableComponent>(component.second);
-                auto textureProps = displayable.get()->getTextureProps();
-                shared::graphics::EntityTextureProps entityTextureProps {
-                    this->_graphicsProvider.get()->createTexture(textureProps.sources.bin, textureProps.sources.ascii),
-                    textureProps.sources.binTileSize,
-                    textureProps.origin
-                };
-                shared::graphics::EntityProps entityProps {
-                    entityTextureProps,
-                    displayable.get()->getSize(),
-                    displayable.get()->getPosition()
-                };
-                this->_window.get()->render(entityProps);
-            }
+            if (component.second.get()->getType() == shared::games::components::DISPLAYABLE)
+                this->_renderDisplayableEntity(std::dynamic_pointer_cast<shared::games::components::IDisplayableComponent>(component.second));
         }
     }
 }
 
 void Core::run()
 {
-    this->_setup();
+    this->_initWindow();
     while (this->_window.get()->isOpen()) {
         this->_window.get()->display();
         this->_window.get()->clear();
--- core/src/core/Core.hpp
+++ core/src/core/Core.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include "types/Providers.hpp"
+#include "shared/games/components/IDisplayableComponent.hpp"
 
 class Core {
     public:
@@ -20,13 +21,13 @@ class Core {
          */
         void run();
 
-
     protected:
     private:
         std::shared_ptr<shared::games::IGame> _game;
         std::shared_ptr<shared::graphics::IWindow> _window;
         std::shared_ptr<shared::games::IGameProvider> _gameProvider;
         std::shared_ptr<shared::graphics::IGraphicsProvider> _graphicsProvider;
+        std::map<std::string, std::shared_ptr<shared::graphics::ITexture>> _textures;
         GameProviders _gameProviders;
         GraphicsProviders _graphicsProviders;
 
@@ -34,11 +35,22 @@ class Core {
          * @brief Initialize the core
          * 
          */
-        void _setup();
+        void _initWindow();
 
         /**
          * @brief Render all entities
          * 
          */
         void _renderEntities();
+
+        /**
+         * @brief Get a texture
+         * 
+         * @param bin Path to the binary file
+         * @param ascii Path to the ascii file
+         * @return The correct texture
+         */
+        std::shared_ptr<shared::graphics::ITexture> _getTexture(std::string bin, std::string ascii);
+
+        void _renderDisplayableEntity(std::shared_ptr<shared::games::components::IDisplayableComponent> displayable);
 };
