b547d8642cdf85da07a239151974fe1d82f8a44f
Flavien Chenu <flavienchenu@hotmail.com>
2024-03-28 16:04:34 +0100

    refactor: update global CMakeLists.txt and restore main.cpp

--- core/main.cpp
+++ core/main.cpp
@@ -7,26 +7,16 @@
 
 #include "loader/DLLoader.hpp"
 
-int main(void) {
-    DLLoader loader;
+int main(void)
+{
+  DLLoader loader;
 
-    try {
-        loader.loadLibraries("./lib");
-        std::cout << "Games libraries: " << loader.getGamesLibraries().size() << std::endl;
-        std::cout << "Graphics libraries: " << loader.getGraphicsLibraries().size() << std::endl;
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-    auto graphics = loader.getGraphicsLibraries();
-    auto provider = graphics[0];
-    auto window = provider->createWindow({
-        {1920, 1080},
-         shared::graphics::IWindow::FULLSCREEN,
-         60,
-         "Notre putain de snake"
-    });
-    while (window->isOpen()) {
-        window->getEvents();
-    }
-    return 0;
+  try {
+    loader.loadLibraries("./lib");
+    std::cout << "Games libraries:" << loader.getGamesLibraries().size() << std::endl;
+    std::cout << "Graphics libraries:" << loader.getGraphicsLibraries().size() << std::endl;
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+  }
+  return 0;
 }
